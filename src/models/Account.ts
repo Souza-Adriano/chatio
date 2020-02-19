8ddc6119b23b6076033c5c2cbec4d4adf1c03b8d
Adriano Souza <azuos.adriano@gmail.com>
2020-02-19 02:53:07 -0300

    :construction: added method validate

--- src/models/Account.ts
+++ src/models/Account.ts
@@ -1,7 +1,7 @@
 import AbstractModel from './AbstractModel';
 import Enigma from '../lib/Enigma';
 
-interface NewUserAccount {
+interface UserAccount {
     id: string;
     name: string;
     nickname: string;
@@ -10,13 +10,28 @@ interface NewUserAccount {
     password: string;
 }
 
+interface User {
+    id: string;
+    name: string;
+    email: string;
+    nickname: string;
+    avatar: string;
+}
+
 interface ExpectedBody {
     create: string[];
+    authenticate: string[];
+}
+
+interface UserAuthenticate {
+    email: string;
+    password: string;
 }
 
 class Account extends AbstractModel {
     private body: ExpectedBody = {
         create: ['name', 'email', 'password', 'nickname', 'confirmPassword'],
+        authenticate: ['email', 'password'],
     };
     private enigma: Enigma;
 
@@ -25,7 +40,7 @@ class Account extends AbstractModel {
         this.enigma = new Enigma();
     }
 
-    private async validateCreation(body: any): Promise<NewUserAccount> {
+    private async validateCreation(body: any): Promise<UserAccount> {
         try {
             this.validator.isValidBody(body, this.body.create);
             this.validator.isEmail(body.email, `${body.email} is not a valid email.`);
@@ -50,14 +65,35 @@ class Account extends AbstractModel {
 
     }
 
+    private async validateAuthentication(body: any): Promise<UserAuthenticate> {
+        this.validator.isValidBody(body, this.body.authenticate);
+        this.validator.isEmail(body.email, `is not a valid email`);
+
+        return body;
+    }
+
     public async create(body: any): Promise<void> {
         try {
-            const UserAccount: NewUserAccount = await this.validateCreation(body);
+            const UserAccount: UserAccount = await this.validateCreation(body);
             await this.database('users')
                 .insert(UserAccount);
         } catch (error) {
             throw new Error(error.message);
         }
+    }
+
+    public async isAuthenticated(body: any): Promise<User> {
+        const data: UserAuthenticate = await this.validateAuthentication(body);
+        const user: UserAccount | undefined = await this.database<UserAccount>('users')
+            .select('id', 'name', 'nickname', 'email', 'avatar', 'password')
+            .where({ email: data.email })
+            .first();
+
+        if (!user) { throw new Error('invalid user/password'); }
+        const isValid: boolean = await this.enigma.validate(data.password, user.password);
+        if (isValid === false) { throw new Error('invalid user/password'); }
+
+        return user;
     }
 }
 
