775438c92eab9ae003e76d635dfde8dbc1032052
Adriano Souza <azuos.adriano@gmail.com>
2020-02-21 00:38:12 -0300

    :sparkles: Service/AbstractSocket

---
+++ src/service/AbstractSocket.ts
@@ -0,0 +1,11 @@
+import { Socket, Server} from 'socket.io';
+
+abstract class AbstractSocket {
+    protected socket: Socket;
+    protected server: Server;
+
+    constructor(socket: Socket, server: Server) {
+        this.socket = socket;
+        this.server = server;
+    }
+}
